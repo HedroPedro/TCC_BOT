ab0c214b25df28577a82fdfda12dcf84d79d6e05
HedroPedro <emailgenericomeu@gmail.com>
2022-02-17 09:03:34 -0300

    Inicilização do repositorio

---
+++ src/main/java/com/bot/App.java
@@ -0,0 +1,14 @@
+package com.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        TelegramBot bot = new TelegramBot("");
+
+
+    }
+}
